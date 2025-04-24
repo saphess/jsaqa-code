cefb49ca26a3f2680b8860ab27f8580372745225
Maria <saphess@mail.ru>
2025-04-24 20:34:27 +0300

    100% coverage

---
+++ 7.3/jest/jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.test.{js,jsx,ts,tsx}",
+  ],
+  coverageReporters: ["text", "html"],
+};
